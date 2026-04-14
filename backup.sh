c08f65b2fbab1ce8841b09570184ecadb94ab7d4
laikamyiu <143649217+laikamyiu@users.noreply.github.com>
2026-04-13 20:21:24 -0400

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+TARGET="/var/log/"
+BACKUP="/home/kamyiusa/proj1/backup"
+DATE=$(date +%Y%m%d)
+
+# we need to make sure that there is a directory we can put the tar file in
+# if the directory is already made this line does not do anything
+mkdir -p "$BACKUP"
+
+# go into the directory from target and create a tar.gz backup
+tar -czf "logs-$DATE.tar.gz" "$TARGET"
+
+# move the backup into the correct backup directory
+mv "logs-$DATE.tar.gz" "$BACKUP"
+
+# in the backup directory find all tar.gz files older than 3 days and delete them
+find "$BACKUP" -type f -name "*.tar.gz" -mtime +3 -delete
